993e5615d2613e6d27809c4526b5c5e5fe61f79b
Preston Carman <preston.carman@wallawalla.edu>
2025-10-21 16:49:22 -0700

    Test empty class bodies

--- titration/ui_state/controller/set_chill_or_heat.py
+++ titration/ui_state/controller/set_chill_or_heat.py
@@ -2,7 +2,6 @@
 The file to hold the Set Chill or Heat class
 """
 
-from titration.devices.library import Keypad
 from titration.ui_state.ui_state import UIState
 
 
@@ -10,25 +9,3 @@ class SetChillOrHeat(UIState):
     """
     This is a class for the SetChillOrHeat state of the Tank Controller
     """
-
-    def __init__(self, titrator, previous_state=None):
-        """
-        Initialize the SetChillOrHeat state
-        :param titrator: The titrator object
-        :param previous_state: The previous state to return to
-        """
-        super().__init__(titrator)
-        self.previous_state = previous_state
-
-    def loop(self):
-        """
-        Main loop for the SetChillOrHeat state
-        """
-        pass
-
-    def handle_key(self, key):
-        """
-        Handle key input for the SetChillOrHeat state
-        """
-        if key == Keypad.KEY_4:  # Left key
-            self._set_next_state(self.previous_state, True)
--- titration/ui_state/controller/set_google_mins.py
+++ titration/ui_state/controller/set_google_mins.py
@@ -2,7 +2,6 @@
 The file to hold the Set Google Sheet Interval class
 """
 
-from titration.devices.library import Keypad
 from titration.ui_state.ui_state import UIState
 
 
@@ -10,25 +9,3 @@ class SetGoogleSheetInterval(UIState):
     """
     This is a class for the SetGoogleSheetInterval state of the Tank Controller
     """
-
-    def __init__(self, titrator, previous_state=None):
-        """
-        Initialize the SetGoogleSheetInterval state
-        :param titrator: The titrator object
-        :param previous_state: The previous state to return to
-        """
-        super().__init__(titrator)
-        self.previous_state = previous_state
-
-    def loop(self):
-        """
-        Main loop for the SetGoogleSheetInterval state
-        """
-        pass
-
-    def handle_key(self, key):
-        """
-        Handle key input for the SetGoogleSheetInterval state
-        """
-        if key == Keypad.KEY_4:  # Left key
-            self._set_next_state(self.previous_state, True)
