f16aee70a8689d48dd581f438db4474c9bc66d89
Samuel Nguyen <57112921+samuelnguyen999@users.noreply.github.com>
2025-10-21 17:10:53 -0700

    Submenu (#5)
    * Edit menu for Tank Controller + test
    
    (cherry picked from commit 96f561d9f6a1ed46637c0c067f46c7c3ad15bcbe)
    
    * black formatting
    
    * add Submenu readme and test
    
    * Update test
    
    * update mainmenu and test
    
    * attempt passing formatting
    
    * Updating doc strings
    
    * Test empty class bodies
    
    * Remove boiler plate code in favor of the default class values from inheritance
    
    * Cleaned up linting
    
    ---------
    
    Co-authored-by: Preston Carman <preston.carman@wallawalla.edu>

--- tests/ui_state/main_menu_test.py
+++ tests/ui_state/main_menu_test.py
@@ -13,7 +13,7 @@ from titration.ui_state.main_menu import MainMenu
 
 
 @mock.patch.object(MainMenu, "_set_next_state")
-def test_handle_key(set_next_state_mock):
+def test_shortcuts(set_next_state_mock):
     """
     The function to test MainMenu's handle_key function for each keypad input
     """
@@ -27,12 +27,25 @@ def test_handle_key(set_next_state_mock):
     set_next_state_mock.assert_called_with(ANY, True)
     assert set_next_state_mock.call_args.args[0].name() == "SetThermalTarget"
 
+
+def test_handle_key_d():
+    """
+    The function to test MainMenu's handle_key function for each keypad input
+    """
+    main_menu = MainMenu(Titrator())
+
     main_menu.level1 = 2
     main_menu.level2 = 5
     main_menu.handle_key("D")
     assert main_menu.level1 == 0
     assert main_menu.level2 == -1
 
+
+def test_handle_up():
+    """
+    The function to test MainMenu's handle_key function for each keypad input
+    """
+    main_menu = MainMenu(Titrator())
     main_menu.level1 = 0
     main_menu.level2 = -1
     main_menu.handle_key("2")
@@ -48,6 +61,13 @@ def test_handle_key(set_next_state_mock):
     main_menu.level2 = 0
     main_menu.handle_key("2")
     assert main_menu.level2 == main_menu.set_command_count - 1
+
+
+def test_handle_left():
+    """
+    The function to test MainMenu's handle_key function for each keypad input
+    """
+    main_menu = MainMenu(Titrator())
 
     main_menu.level1 = 1
     main_menu.level2 = -1
@@ -67,6 +87,13 @@ def test_handle_key(set_next_state_mock):
     assert main_menu.level1 == 2
     assert main_menu.level2 == -1
 
+
+@mock.patch.object(MainMenu, "_set_next_state")
+def test_handle_right(set_next_state_mock):
+    """
+    The function to test MainMenu's handle_key function for each keypad input
+    """
+    main_menu = MainMenu(Titrator())
     main_menu.level1 = 0
     main_menu.level2 = -1
     main_menu.handle_key("6")
@@ -90,6 +117,12 @@ def test_handle_key(set_next_state_mock):
     set_next_state_mock.assert_called_with(ANY, True)
     assert main_menu.level1 == 2
 
+
+def test_handle_down():
+    """
+    The function to test MainMenu's handle_key function for each keypad input
+    """
+    main_menu = MainMenu(Titrator())
     main_menu.level1 = 0
     main_menu.level2 = -1
     main_menu.handle_key("8")
@@ -108,54 +141,55 @@ def test_handle_key(set_next_state_mock):
 
 
 @mock.patch.object(LiquidCrystal, "print")
-def test_loop(print_mock):
+def test_view_list(print_mock):
     """
-    The function to test MainMenu's loop function's LiquidCrystal calls
+    The function to test navigation to submenu view features
     """
     main_menu = MainMenu(Titrator())
 
     main_menu.loop()
     print_mock.assert_any_call("Idle Line 1", line=1)
     print_mock.assert_any_call("Idle Line 2", line=2)
-
     print_mock.reset_mock()
 
-    main_menu.level1 = 1
-    main_menu.level2 = -1
+    main_menu.handle_key("6")
     main_menu.loop()
-    print_mock.assert_has_calls(
-        [mock.call("View settings", line=1), mock.call("<4   ^2  8v   6>", line=2)]
-    )
-
+    print_mock.assert_any_call("View settings", line=1)
+    print_mock.assert_any_call("<4   ^2  8v   6>", line=2)
     print_mock.reset_mock()
 
-    main_menu.level1 = 1
-    main_menu.level2 = 2
-    main_menu.loop()
-    print_mock.assert_has_calls(
-        [
-            mock.call(main_menu.view_menus[2], line=1),
-            mock.call("<4   ^2  8v   6>", line=2),
-        ]
-    )
+    main_menu.handle_key("6")
+    for index, label in enumerate(main_menu.view_menus):
+        print_mock.reset_mock()
+        if index != 0:
+            main_menu.handle_key("8")
+        main_menu.loop()
+        print_mock.assert_any_call(label, line=1)
 
+
+@mock.patch.object(LiquidCrystal, "print")
+def test_change_list(print_mock):
+    """
+    The function to test navigation to submenu set features
+    """
+    main_menu = MainMenu(Titrator())
+
+    main_menu.loop()
+    print_mock.assert_any_call("Idle Line 1", line=1)
+    print_mock.assert_any_call("Idle Line 2", line=2)
     print_mock.reset_mock()
 
-    main_menu.level1 = 2
-    main_menu.level2 = -1
+    main_menu.handle_key("6")
+    main_menu.handle_key("8")
     main_menu.loop()
-    print_mock.assert_has_calls(
-        [mock.call("Change settings", line=1), mock.call("<4   ^2  8v   6>", line=2)]
-    )
-
+    print_mock.assert_any_call("Change settings", line=1)
+    print_mock.assert_any_call("<4   ^2  8v   6>", line=2)
     print_mock.reset_mock()
 
-    main_menu.level1 = 2
-    main_menu.level2 = 3
-    main_menu.loop()
-    print_mock.assert_has_calls(
-        [
-            mock.call(main_menu.set_menus[3], line=1),
-            mock.call("<4   ^2  8v   6>", line=2),
-        ]
-    )
+    main_menu.handle_key("6")
+    for index, label in enumerate(main_menu.set_menus):
+        print_mock.reset_mock()
+        if index != 0:
+            main_menu.handle_key("8")
+        main_menu.loop()
+        print_mock.assert_any_call(label, line=1)
