a8683935b1c60eacefd724781a7b2537cd7912bf
Samuel Nguyen <samuelnguyen316@gmail.com>
2025-10-24 08:05:57 -0700

    add view google minutes feature

--- titration/ui_state/controller/view_google_mins.py
+++ titration/ui_state/controller/view_google_mins.py
@@ -1,7 +1,7 @@
 """
 The file to hold the View Google Mins class
 """
-
+from titration.devices.library import Keypad
 from titration.ui_state.ui_state import UIState
 
 
@@ -9,3 +9,16 @@ class ViewGoogleMins(UIState):
     """
     This is a class for the ViewGoogleMins state of the Tank Controller
     """
+    def __init__(self, titrator, previous_state=None):
+        super().__init__(titrator)
+        self.previous_state = previous_state
+        self.google_interval_address = 108
+
+    def loop(self):
+        self.titrator.lcd.print("Google Mins:", line=1)
+        self.titrator.lcd.print(f"{self.google_interval_address} mins", line=2)
+
+    def handle_key(self, key):
+        # Handle key input for this state
+        if key == Keypad.KEY_4 or key == Keypad.KEY_D:
+            self._set_next_state(self.previous_state, True)
