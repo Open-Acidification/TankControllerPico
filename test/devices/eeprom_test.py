910807ff4b95a65192b4b03ff46021de2c947166
Samuel Nguyen <samuelnguyen316@gmail.com>
2025-10-27 13:35:15 -0700

    add set google sheet interval value test

--- test/devices/eeprom_test.py
+++ test/devices/eeprom_test.py
@@ -5,9 +5,17 @@ The file to test the EEPROM class
 from titration.devices.eeprom import EEPROM
 
 
-def test_default_google_sheet_interval():
+def test_default_google_sheet_interval_value():
     """
     The function to test the default google_sheet_interval value
     """
     e = EEPROM()
     assert e.google_sheet_interval == 20
+
+def test_set_google_sheet_interval_value():
+    """
+    The function to test setting the google_sheet_interval value
+    """
+    e = EEPROM()
+    e.google_sheet_interval = 45
+    assert e.google_sheet_interval == 45
