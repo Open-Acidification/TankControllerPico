f9302f98942dc70a4b1c0164429c3d281c1980ab
Samuel Nguyen <samuelnguyen316@gmail.com>
2025-11-07 14:24:24 -0800

    format

--- test/devices/sd_test.py
+++ test/devices/sd_test.py
@@ -9,7 +9,7 @@ def test_sd_todays_data_file_name():
     """
     The function to test the default google_sheet_interval value
     """
-    sd = SD()
-    file_name = sd.todays_data_file_name()
+    sd_device = SD()
+    file_name = sd_device.todays_data_file_name()
     assert file_name.startswith("2025")
     assert file_name.endswith(".csv")
--- titration/ui_state/controller/view_log_file.py
+++ titration/ui_state/controller/view_log_file.py
@@ -16,9 +16,15 @@ class ViewLogFile(UIState):
         self.previous_state = previous_state
 
     def loop(self):
+        """
+        The loop function for the ViewLogFile state
+        """
         self.titrator.lcd.print("Current Log File", line=1)
         self.titrator.lcd.print(f"{self.titrator.sd.todays_data_file_name()}", line=2)
 
     def handle_key(self, key):
-        if key == Keypad.KEY_4 or key == Keypad.KEY_D:
+        """
+        The handle_key function for the ViewLogFile state
+        """
+        if key in (Keypad.KEY_4, Keypad.KEY_D):
             self._set_next_state(self.previous_state, True)
