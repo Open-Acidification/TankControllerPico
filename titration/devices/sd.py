e552f36f924e753257c3e4e4282a4bda5e1c7b05
Samuel Nguyen <samuelnguyen316@gmail.com>
2025-11-07 13:42:37 -0800

    delete unused sd class objects

--- titration/devices/sd.py
+++ titration/devices/sd.py
@@ -13,27 +13,7 @@ class SD:
     inspect or remove.
     """
 
-    def __init__(self):
-        """The constructor for the mock SD class."""
-        self.most_recent_data_log_header = ""
-        self.most_recent_data_log_line = ""
-
     def todays_data_file_name(self) -> str:
         """Return today's data filename as YYYYMMDD.csv"""
         now = datetime.now()
         return f"{now.year:04d}{now.month:02d}{now.day:02d}.csv"
-
-    def write_to_data_log(self, header: str, line: str) -> None:
-        """Create today's data file (or append) and write header + line.
-        """
-        self.most_recent_data_log_header = str(header)[:128]
-        self.most_recent_data_log_line = str(line)[:128]
-        fname = self.todays_data_file_name()
-        p = self.data_logs / fname
-
-        if not p.exists():
-            with p.open("w", encoding="utf-8") as f:
-                if header:
-                    f.write(header + "\n")
-        with p.open("a", encoding="utf-8") as f:
-            f.write(line + "\n")
