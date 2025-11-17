2976d7c5b924095e52b96ada75ef3a74e745168b
Samuel Nguyen <57112921+samuelnguyen999@users.noreply.github.com>
2025-11-17 15:28:18 -0800

    View log file (#9)
    * add view log file feature with test. add sd class
    
    * adjust SD class
    
    * delete unused sd class objects
    
    * format
    
    * format
    
    * format sd -> sd_device
    
    * black formatting
    
    * edit test to check whole date string

--- titration/ui_state/controller/view_log_file.py
+++ titration/ui_state/controller/view_log_file.py
@@ -2,6 +2,7 @@
 The file to hold the View Log File class
 """
 
+from titration.devices.library import Keypad
 from titration.ui_state.ui_state import UIState
 
 
@@ -9,3 +10,23 @@ class ViewLogFile(UIState):
     """
     This is a class for the ViewLogFile state of the Tank Controller
     """
+
+    def __init__(self, titrator, previous_state=None):
+        super().__init__(titrator)
+        self.previous_state = previous_state
+
+    def loop(self):
+        """
+        The loop function for the ViewLogFile state
+        """
+        self.titrator.lcd.print("Current Log File", line=1)
+        self.titrator.lcd.print(
+            f"{self.titrator.sd_device.todays_data_file_name()}", line=2
+        )
+
+    def handle_key(self, key):
+        """
+        The handle_key function for the ViewLogFile state
+        """
+        if key in (Keypad.KEY_4, Keypad.KEY_D):
+            self._set_next_state(self.previous_state, True)
